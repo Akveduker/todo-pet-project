10c7fb1b4a4f33f1d2aec09ceb9086372afc5352
Nick akveduk <nikkap2001@mail.ru>
2023-08-21 14:31:42 +0300

    Пределал хук для аунтефикации и регистрации
    Теперь хук помимо добавления юзера в стор сохраняет в куках токен

--- src/hooks/authorization/useSetAuthState.ts
+++ src/hooks/authorization/useSetAuthState.ts
@@ -1,15 +1,19 @@
 import { useEffect } from "react"
 import { setUser } from "store/reducers/userSlice/userSlice"
 import { useAppDispatch } from "store/store"
+import { AuthReturnType } from "types/auth/auth"
 import { DataFetch } from "types/dataFetch/dataFetch"
-import { User } from "types/user/user"
+import Cookies from 'js-cookie'
+import { storageNames } from "utils/constants/storageNames/storageNames"
 import { VAL_SUCCESS } from "utils/constants/validationTypes/validationTypes"
 
-export const useSetAuthState = (result: DataFetch<User>) => {
+export const useSetAuthState = (result: DataFetch<AuthReturnType>) => {
     const dispatch = useAppDispatch()
+    const { fetchStatus } = result
     useEffect(() => {
-        if (result.fetchStatus === VAL_SUCCESS) {
-            dispatch(setUser(result.data))
+        if (fetchStatus === VAL_SUCCESS) {
+            Cookies.set(storageNames.token, result.data.token)
+            dispatch(setUser(result.data.data))
         }
-    }, [result.fetchStatus, dispatch])
+    }, [fetchStatus, dispatch])
 }
