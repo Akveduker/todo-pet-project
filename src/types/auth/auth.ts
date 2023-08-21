706e9930f0083564f7a82340118b998586bf2586
Nick akveduk <nikkap2001@mail.ru>
2023-08-21 14:24:46 +0300

    Новые типы для респонса регистрации и авторизации

--- src/types/auth/auth.ts
+++ src/types/auth/auth.ts
@@ -3,4 +3,12 @@ import { User } from "types/user/user";
 export type AuthDataReq = Pick<User, 'email' | 'password'>
 
 export type RegDataReq = Pick<User, 'userColor' | 'name'> & AuthDataReq
-export type RegDataReqFc = (email: string, password: string, name: string) => RegDataReq 
+
+export type RegDataReqFc = (email: string, password: string, name: string) => RegDataReq
+
+export type JwtToken = string
+
+export interface AuthReturnType {
+    token: JwtToken,
+    data: User,
+}
